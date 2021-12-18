b0678be62ff43ed23072f1de8dbe4d39cc5daed6
Brandon-mg <90120777+Brandon-mg@users.noreply.github.com>
2021-12-18 23:19:11 +0530

    MCMPDRP python file

---
+++ MCMPDRP.py
@@ -0,0 +1,32 @@
+from pypresence import Presence
+import time
+import requests
+import json
+#discord api id
+apiID = "" 
+if apiID=="":
+    print("please add the required details")
+    return
+RPC = Presence(apiID)
+RPC.connect()
+#server ip
+ip=""
+#server port
+port="25565"
+url="https://mcapi.us/server/status?ip="+ip+"&port="+port
+#server online/offline message
+richPresOnline = "Server is Online {}/{}"
+richPresOffline = "Server is Offline"
+while True:
+    response = requests.get(url)
+    serverStats = json.loads(response.text)
+    online = serverStats["status"]
+    maxPlayers = serverStats["players"]["max"]
+    curPlayers = serverStats["players"]["now"]
+    if(online=="success"):
+        status=richPresOnline.format(curPlayers,maxPlayers)
+    else:
+        status=richPresOffline
+    print(status)
+    RPC.update(state=status)
+    time.sleep(300)
